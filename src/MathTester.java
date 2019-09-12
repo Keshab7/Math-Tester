9605d65605876cee3ba065eefe71667c2953748f
Keshab <keshab.bista.2020@student.lhschools.org>
2019-09-12 09:38:37 -0400

    Deleted "Hello World"

--- src/MathTester.java
+++ src/MathTester.java
@@ -2,7 +2,6 @@ public class MathTester {
 
     public static void main(String[] args) {
         String firstWord
-                System.out.print("Hello World");
-        System.out.print("Yessir");
+
     }
 }
