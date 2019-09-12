14f9107c326d0f633d744982de6b60a659bc5655
Keshab <keshab.bista.2020@student.lhschools.org>
2019-09-12 10:32:24 -0400

    Commented out the math operations

--- src/MathTester.java
+++ src/MathTester.java
@@ -9,16 +9,17 @@ public class MathTester {
 
         firstWord = "dog";
         secondWord = "house";
+        
         firstNumber = 5;
         secondNumber = 10;
 
-        System.out.println(firstWord+secondWord);
+       /* System.out.println(firstWord+secondWord);
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNuber is " + (firstNumber+secondNumber));
         System.out.println("firstNumber - secondNuber is " + (firstNumber-secondNumber));
         System.out.println("secondNumber / firstNuber is " + (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNuber is " + (firstNumber*secondNumber));
         System.out.println("The modulus of the secondNumber by the firstnumber is " + (secondNumber%firstNumber));
-
+*/
     }
 }
