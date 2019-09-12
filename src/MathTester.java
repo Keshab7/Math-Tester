6a028e15d6ad34c79db6881b5c34eb583f9bf5a6
Keshab7 <55144983+Keshab7@users.noreply.github.com>
2019-09-12 09:56:23 -0400

    Added Hello World

--- src/MathTester.java
+++ src/MathTester.java
@@ -2,10 +2,6 @@ public class MathTester {
 
     public static void main(String[] args) {
         String firstWord
-            
-            
-            
-            
-
+System.out.println("Hello World");
     }
 }
