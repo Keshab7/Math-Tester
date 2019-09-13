14f6a4786b22bae94233f85e94bab22ab7aa5dfc
Keshab <keshab.bista.2020@student.lhschools.org>
2019-09-13 09:32:46 -0400

    Added Totals

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,16 +3,18 @@ public class MathTester {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
-        int firstNumber,secondNumber;
+        int firstNumber,secondNumber, total;
 
 
 
         firstWord = "dog";
         secondWord = "house";
-        
         firstNumber = 5;
         secondNumber = 10;
-
+        total = firstNumber+secondNumber;
+        System.out.println(total);
+    total = total + 10;
+        System.out.println(total);
        /* System.out.println(firstWord+secondWord);
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNuber is " + (firstNumber+secondNumber));
